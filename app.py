5b8fbcd8a431bcfe792e759fb8363f993a004a42
SkymePC <skyme@localhost.localdomain>
2021-02-25 19:21:05 +0300

    debug is off

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
