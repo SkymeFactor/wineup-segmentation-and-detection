e372e5e25c63a93b86dd2a5e6da2e2e6939ca232
Albert <3litac@GMAIL.COM>
2021-04-08 13:59:14 +0300

    Miscellaneous code refactoring

--- kafka_controller.py
+++ kafka_controller.py
@@ -1,73 +1,77 @@
 from __future__ import unicode_literals
 
-import NewSegmentationIsReadyEvent_pb2 as msg
+import logging
+import typing
+
 from kafka import KafkaConsumer, KafkaProducer
 
-topic = 'eventTopic'
+import NewSegmentationIsReadyEvent_pb2 as msg
 
-log = []
 
-def connect_kafka_producer():
-    _producer = None
+EVENT_TOPIC = 'eventTopic'
+
+log = logging.getLogger(__name__)
+
+
+def connect_kafka_producer() -> KafkaProducer:
     try:
-        _producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-    except Exception:
-        log.append('producer connection failed')
-        pass
-    finally:
-        log.append('producer connection success')
-        return _producer
+        producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+    except Exception as error:
+        log.error("Producer connection failed", exc_info=error)
+        raise
+    log.info("Producer connection success")
+    return producer
 
 
-def publish_message(producer_instance, topic_name, data):
+def publish_message(
+        producer: KafkaProducer, topic_name: str, data: msg.NewSegmentationIsReadyEvent
+) -> None:
     try:
-        #key_bytes = bytes(str(key), encoding='utf-8')
-        #value_bytes = bytes(str(value), encoding='utf-8')
-        #print(data)
-        producer_instance.send(topic_name, data.SerializeToString())#key=key_bytes, value=value_bytes)
-        producer_instance.flush()
-        log.append('Message successfully sent')
-    except Exception as e:
-        log.append(str(e))
+        # key_bytes = bytes(str(key), encoding='utf-8')
+        # value_bytes = bytes(str(value), encoding='utf-8')
+        # print(data)
+
+        # key=key_bytes, value=value_bytes)
+        producer.send(topic_name, data.SerializeToString())
+        producer.flush()
+        log.info("Message successfully sent")
+    except Exception as error:
+        log.error("Failed to publish message", exc_info=error)
 
 
-def get_message(consumer):
+def get_message(consumer: KafkaConsumer) -> typing.List[typing.Any]:
     links = []
     for message in consumer:
         value = message.value
         link = msg.NewSegmentationIsReadyEvent()
         link.ParseFromString(value)
-        log.append('Message successfully received')
-        #log.append(str(link))
+        log.info("Message successfully received: %s", link)
+        # log.info(str(link))
         links.append(link)
         return links
 
-def kafka_try_send():
+
+def kafka_try_send() -> None:
     producer = connect_kafka_producer()
-    consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest')
+    consumer = KafkaConsumer(
+        EVENT_TOPIC, bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest'
+    )
 
     segm = msg.NewSegmentationIsReadyEvent()
 
     segm.segmLink = "http://nowhere"
     segm.maskLink = "http://non-existent"
-    segm.wineId   = "1"
+    segm.wineId = "1"
 
-    #serialized = segm.SerializeToString()
+    # serialized = segm.SerializeToString()
 
-    log.append(str(segm))
+    log.info("Sent event %s", segm)
 
-    publish_message(producer, topic, segm) #'msg', str(serialized))
-    links = get_message(consumer)
+    publish_message(producer, EVENT_TOPIC, segm)  # 'msg', str(serialized))
+    get_message(consumer)
 
-    log.append(str(links[0]))
 
-    d = {}
-    for i in range(len(log)):
-        d.update({'line ' + str(i): log[i]})
-
-    return d
-
-print(kafka_try_send())
+kafka_try_send()
 
 # Test that the protobuf serialize and de-serialize methods are working fine
 '''
